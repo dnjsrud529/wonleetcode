2264e9c1db4d1f74747f9d1dc56a541afe623dfb
wonkyoung <wonkyoung@codemind.co.kr>
2021-12-02 06:48:48 +0900

    add searchInsertPosition

---
+++ src/main/scala/searchInsertPosition.scala
@@ -0,0 +1,20 @@
+object searchInsertPosition {
+  def searchInsert(nums: Array[Int], target: Int): Int = {
+    var ind = -1
+    var cnt = 0
+
+    nums.foreach(num => {
+      if(target <= num && ind == -1)
+        ind = cnt
+
+      cnt += 1
+    })
+
+    if(ind == -1)
+      ind = cnt
+
+    ind
+
+  }
+
+}
